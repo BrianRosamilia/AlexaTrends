a0f34e86b58b19e10ff5e8f856aa3718c9ca106e
Brian Rosamilia <brian.rosamilia@gmail.com>
2016-07-15 23:10:34 -0400

    add redis

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 "use strict";
 let alexa = require('alexa-app');
 let app = new alexa.app();
-let requestify = require('requestify');
+let request = require('request-promise');
+let redis = require('redis');
+let promise = require('bluebird');
+// promise.config({
+//     longStackTraces: false,
+//     warnings: false
+// });
+let config = require('config').get('Amazon');
+
+promise.promisifyAll(redis.RedisClient.prototype);
+promise.promisifyAll(redis.Multi.prototype);
 
 app.launch(function (request, response) {
-    //response.say('<speak>Here are todays trending topics <break time="5s"/></speak>');
     response.say('<speak>Here are todays trending topics<break time="1s"/>');
 
     ProcessRequest('all', response);
@@ -16,44 +25,67 @@ app.intent('getTrendsByCategory',
     {},
     function (request, response) {
         var category = request.slot('category');
+        let categoryParam = mapCategoryToParam(category);
 
-        console.log(typeof response.response.response.outputSpeech);
+        if(categoryParam == 'help'){
+            response.say('<speak>Try asking trends about technology, business, health, or sports.  Or you can just say "alexa <break time="200ms"/>trends');
+            return renderResponse(response);
+        }
         response.say(`<speak>Trending in ${category}<break time="1s"/>`);
 
-        ProcessRequest(category, response);
+        ProcessRequest(categoryParam, response);
 
         return false;
     }
 );
 
 app.error = function (exception, request, response) {
+    console.log('Alex global error handler', exception);
     response.say('Sorry, something bad happened');
+    renderResponse(response);
 };
 
-function ProcessRequest(category, awsResponse) {
-    let categoryParam = mapCategoryToParam(category);
+function ProcessRequest(categoryParam, awsResponse) {
+    // let client = redis.createClient({ host : config.redis, port : config.redisPort, connect_timeout : 2000 });
+    //
+    // client.on("error", function (err) {
+    //     console.log("Redis Client error " + err);
+    // });
+    //
+    // client.setAsync(['ttl key', 'ttl val']).then(function(){
+    //     console.log('key set');
+    // }).then(function(){
+    //     console.log('enter requestify');
+    //     return
+    request({ uri: `https://www.google.com/trends/api/stories/latest?hl=en-US&cat=${categoryParam}&fi=15&fs=15&geo=US&ri=300&rs=8&sort=0&tz=240`})
+        .then(function (response) {
+            console.log('requestify response received');
+            var responseText = spliceSlice(response.getBody(), 0, 4);
+            var responseJSON = JSON.parse(responseText);
+            let updateSpeech = responseJSON.storySummaries.trendingStories.map(function (x) {
+                return x.title.split(',')[0]
+            });
 
-    requestify.get(`https://www.google.com/trends/api/stories/latest?hl=en-US&cat=${categoryParam}&fi=15&fs=15&geo=US&ri=300&rs=8&sort=0&tz=240`).then(function (response) {
-        // Get the response body
-        var responseText = spliceSlice(response.getBody(), 0, 4);
-        var responseJSON = JSON.parse(responseText);
-        let updateSpeech = responseJSON.storySummaries.trendingStories.map(function (x) {
-            return x.title.split(',')[0]
-        });
-
-        updateSpeech.splice(updateSpeech.length-1, 0, 'and <break time="500ms"/>');
-        console.log('Speaking', updateSpeech);
-        awsResponse.say(updateSpeech);
+            updateSpeech.splice(updateSpeech.length-1, 0, 'and <break time="500ms"/>');
+            console.log('Speaking', updateSpeech);
+            awsResponse.say(updateSpeech);
+        // })
+    }).then(function(){
+        console.log('finish requestify');
     }).catch(function (e) {
         console.log(e);
-        awsResponse.say('Sorry, something bad happened');
+        awsResponse.say('Sorry, something bad happened while getting trends');
+        renderResponse(awsResponse);
     }).finally(function(){
-        awsResponse.say('</speak>');
-        awsResponse.response.response.outputSpeech.type = 'SSML';
-        awsResponse.response.response.outputSpeech.ssml = awsResponse.response.response.outputSpeech.text;
-
-        awsResponse.send();
+        renderResponse(awsResponse);
     });
+}
+
+function renderResponse(awsResponse){
+    awsResponse.say('</speak>');
+    awsResponse.response.response.outputSpeech.type = 'SSML';
+    awsResponse.response.response.outputSpeech.ssml = awsResponse.response.response.outputSpeech.text;
+    awsResponse.send();
 }
 
 function mapCategoryToParam(word){
@@ -67,7 +99,7 @@ function mapCategoryToParam(word){
         case 'sports' :
             return 's';
         default :
-            return 'all';
+            return 'help';
     }
 }
 
